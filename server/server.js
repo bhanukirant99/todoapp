6aaa46574ffac0373e4da507309fa88e28e10e9b
bhanukirant99 <bhanukirant99@gmail.com>
2020-08-17 14:14:41 +0530

    adding models and db

--- server/server.js
+++ server/server.js
@@ -2,37 +2,108 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+// const database = require('./src/models/user');
+const User = require('./src/models/user');
+const Todo = require('./src/models/todo');
+require('./src/db/mongodb');
 
 const app = new express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '101',
-            name: 'Raj',
-            email: 'raj@gmail.com',
-            password: 'raj',
-            entries: 0,
-            joined: new Date(),
-            todos: [],
-        },
-        {
-            id: '102',
-            name: 'Ram',
-            email: 'ram@gmail.com',
-            password: 'ram',
-            entries: 0,
-            joined: new Date(),
-            todos: [],
-        },
-    ]
-}
+// const database = {
+//     users: [
+//         {
+//             id: '101',
+//             name: 'Raj',
+//             email: 'raj@gmail.com',
+//             password: 'raj',
+//             entries: 0,
+//             joined: new Date(),
+//             todos: [],
+//         },
+//         {
+//             id: '102',
+//             name: 'Ram',
+//             email: 'ram@gmail.com',
+//             password: 'ram',
+//             entries: 0,
+//             joined: new Date(),
+//             todos: [],
+//         },
+//     ]
+// }
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send(database);
+    console.log(database)
+
+});
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/todos', (req, res) => [
+    Todo.find({}).then((todos) => {
+        res.send(todos)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+])
+
+app.get('/todos/:id', (req, res) => {
+    const _id = req.params.id
+    Todo.findById(_id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send()
+        }
+        res.send(todo)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+
+app.post('/signin', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+});
+
+app.get('/signin/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((error) => {
+        res.status(500).send()
+    })
 });
 
 app.post('/signin', (req, res) => {
@@ -47,19 +118,28 @@ app.post('/signin', (req, res) => {
     res.json('signing');
 });
 
+// app.post('/register', (req, res) => {
+//     const { name, email, password } = req.body;
+//     bcrypt.hash(password, null, null, function(err, hash) {
+//         console.log(hash);
+//     });
+//     database.push({
+//             id: '103',
+//             name: name,
+//             email: email,
+//             entries: 0,
+//             joined: new Date(),
+//     })
+//     res.json(database.users[database.length-1]);
+// });
+
 app.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash);
-    });
-    database.users.push({
-            id: '103',
-            name: name,
-            email: email,
-            entries: 0,
-            joined: new Date(),
-    })
-    res.json(database.users[database.users.length-1]);
+   const user = new User(req.body)
+   user.save().then(() => {
+       res.send(user)
+   }).catch((error) => {
+       res.status(400).send(error.message)
+   })
 });
 
 app.get('/profile/:id', (req, res) => {
@@ -89,18 +169,28 @@ app.put('/entries', (req, res) => {
     }
 });
 
+// app.post('/addtodo', (req, res) => {
+//     const { id, todo } = req.body;
+//     let found = false;
+//     database.users.forEach(user => {
+//         if(user.id === id) {
+//             user.todos.push(todo);
+//             return res.json(user.todos);
+//         }
+//     });
+//     if (!found) {
+//         res.status(404).json('no user found!');
+//     }
+// });
+
 app.post('/addtodo', (req, res) => {
-    const { id, todo } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            user.todos.push(todo);
-            return res.json(user.todos);
-        }
-    });
-    if (!found) {
-        res.status(404).json('no user found!');
-    }
+    const todo = new Todo(req.body);
+    todo.save().then(() => {
+        res.send(todo)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+
 });
 
 app.post('/updatetodo', (req, res) => {
